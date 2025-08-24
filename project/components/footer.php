10517092d5c6a89bc0900468729926f4fa68be60
sushmag0wda <sushmagowda636@gmail.com>
2025-08-24 18:09:38 +0530

    Initial commit of MajesticStay project

---
+++ project/components/footer.php
@@ -0,0 +1,34 @@
+<!-- footer section starts  -->
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:XXXXXXXXXX"><i class="fas fa-phone"></i> +XXX-XXX-XXXX</a>
+         <a href="tel:XXXXXXXXXX"><i class="fas fa-phone"></i> +XXX-XXX-XXXX</a>
+         <a href="abc@gmail.com"><i class="fas fa-envelope"></i> abc@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Bengaluru, india </a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+      </div>
+
+   </div>
+
+   <p class="credit">Designed with <i class="fa fa-heart pulse" style="color: red;"></i> by <a href="https://www.linkedin.com/in/sushma-gowda-845b90339/"> sushma gowda</a></p>
+</section>
+
+<!-- footer section ends -->
